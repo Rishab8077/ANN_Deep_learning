96f091874033e97810c08d87b5e8779ac2f4d46e
Rishab8077 <rishabbothra56@gmail.com>
2021-09-04 01:00:56 +0545

    temp

--- temp.py
+++ temp.py
@@ -20,3 +20,66 @@ X=X.drop(['Geography','Gender'],axis=1)
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+# units is output and kernel_initializer is weight intialization technique
+classifier.add(Dense(units = 10,kernel_initializer='he_uniform',activation='relu',input_dim = 11))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 10, kernel_initializer = 'he_uniform',activation='relu'))
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'glorot_uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.summary()
+
+model_history=classifier.fit(X_train, y_train,validation_split=0.33, batch_size = 100,epochs = 100)
+
+
+# list all data in history
+
+print(model_history.history.keys())
+# summarize history for accuracy
+plt.plot(model_history.history['accuracy'])
+plt.plot(model_history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+cm
+
+# Calculate the Accuracy
+from sklearn.metrics import accuracy_score
+score=accuracy_score(y_pred,y_test)
